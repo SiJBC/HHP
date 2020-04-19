b6b146cefbefa8a3c31743f76ba8444d6592a034
Simon Colman <s_colman@hotmail.com>
2020-04-19 10:41:23 +1000

    initial commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: process.env.MONGODB_URI || "mongodb://<user1><password1>@ads125871.mlab.com25871/heroku_0"
+  // "mongodb+srv://harsh:usyd2020@facebooklogin-imntv.mongodb.net/test?retryWrites=true&w=majority" ,
+  secretOrKey: "secret"
+};
