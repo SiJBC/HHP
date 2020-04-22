ea2b5d460cee6e17cc5f25256d786753b0036903
Simon Colman <s_colman@hotmail.com>
2020-04-22 11:08:08 +1000

    deploy

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGODB_URI || "mongodb://localhost/healthyhumanproject" ,
+  mongoURI: process.env.MONGODB_URI || "mongodb://mongodb://<user1><password1>@ads125871.mlab.com25871/heroku_0" ,
   // "mongodb://<user1><password1>@ads125871.mlab.com25871/heroku_0"
-  
+  // localhost/healthyhumanproject
   secretOrKey: "secret"
 };
