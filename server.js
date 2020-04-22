e6532b5e68178c1357fff40677a55ef139921cce
Simon Colman <s_colman@hotmail.com>
2020-04-22 20:26:47 +1000

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-
+var path = require("path");
 const users = require("./routes/api/users");
 const Ailments = require("./routes/api/Ailments")
 const app = express();
